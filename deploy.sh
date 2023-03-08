2facd03ad5208f50c67fb04fdc23413b225f34ce
jayasrikusuma <116686409+jayasrikusuma@users.noreply.github.com>
2023-03-08 22:57:28 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
 pm2 stop example_app
-cd ExampleApplication/
+cd SimpleApplication/
 npm install
 pm2 start ./bin/www --name example_app --env=production
