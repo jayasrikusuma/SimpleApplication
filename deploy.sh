15e21cf12bd75e98d429dd791d0b9a505d24dcb3
jayasrikusuma <116686409+jayasrikusuma@users.noreply.github.com>
2023-03-08 23:18:46 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
-pm2 stop SimpleApplication
+pm2 stop SimpleApp
 cd SimpleApplication/
 npm install
-pm2 start ./bin/www --name SimpleApplication --env=production
+pm2 start ./bin/www --name SimpleApp --env=production
